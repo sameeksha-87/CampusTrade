dc25438b9bfc3ab95d76de578f44503421659604
sameeksha <sameekshaks87@gmail.com>
2026-06-24 21:06:32 +0530

    debugging

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,9 +1,16 @@
+const dns = require("dns");
+
+dns.setDefaultResultOrder("ipv4first");
+
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+
+  family: 4, 
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
